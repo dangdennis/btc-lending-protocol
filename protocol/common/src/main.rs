acdcf93e8b23270619f5c31cbf92ba329a5c6495
Dennis Dang <dangggdennis@gmail.com>
2022-06-12 14:10:03 -0400

    setup new protcol directory for common and motoko

---
+++ protocol/common/src/main.rs
@@ -0,0 +1,139 @@
+//! A simple candid API for Motoko canisters
+//!
+//! Some fundamental crypto primitives are still missing in Motoko, and that
+//! makes some fundamental operations, like computing a Bitcoin address and
+//! signing a transaction impossible in Motoko.
+//!
+//! The following is a work-around until the crypto primitives become available
+//! to canisters. We wrap all the functionality requiring cryptography into a
+//! canister, and Motoko developers can deploy this canister and interact with it
+//! for address computation and transaction signing.
+use bitcoin::{
+    consensus::deserialize, util::psbt::serialize::Serialize, Address, Network, PrivateKey,
+    Transaction,
+};
+use common::types::{BuildTransactionError, SignTransactionError};
+use ic_btc_types::{OutPoint, Utxo};
+use ic_cdk::export::candid::{candid_method, CandidType, Deserialize};
+use ic_cdk_macros::query;
+use std::{collections::HashMap, str::FromStr};
+
+#[derive(CandidType, Deserialize, Copy, Clone)]
+pub enum NetworkCandid {
+    Bitcoin,
+    Regtest,
+    Testnet,
+    Signet,
+}
+
+// Returns the P2PKH address of the given private key.
+#[query]
+#[candid_method(query)]
+fn get_p2pkh_address(private_key_wif: String, network: NetworkCandid) -> String {
+    let private_key = PrivateKey::from_wif(&private_key_wif).expect("Invalid private key WIF");
+    let network = match network {
+        NetworkCandid::Bitcoin => Network::Bitcoin,
+        NetworkCandid::Testnet => Network::Testnet,
+        NetworkCandid::Regtest => Network::Regtest,
+        NetworkCandid::Signet => Network::Signet,
+    };
+    common::get_p2pkh_address(&private_key, network).to_string()
+}
+
+// Returns the transaction as serialized bytes and the UTXO indices used for the transaction.
+#[query]
+#[candid_method(query)]
+fn build_transaction(
+    utxos: Vec<Utxo>,
+    source_address: String,
+    destination_address: String,
+    amount: u64,
+    fees: u64,
+) -> Result<(Vec<u8>, Vec<usize>), BuildTransactionError> {
+    let source_address = Address::from_str(&source_address)
+        .map_err(|_| BuildTransactionError::MalformedSourceAddress)?;
+    let destination_address = Address::from_str(&destination_address)
+        .map_err(|_| BuildTransactionError::MalformedDestinationAddress)?;
+    let outpoint_to_index: HashMap<OutPoint, usize> = utxos
+        .iter()
+        .enumerate()
+        .map(|(idx, utxo)| (utxo.outpoint.clone(), idx))
+        .collect();
+    let tx = common::build_transaction(
+        utxos,
+        source_address.clone(),
+        destination_address,
+        amount,
+        fees,
+    )
+    .map_err(|_| BuildTransactionError::InsufficientBalance)?;
+    let used_utxo_indices: Vec<usize> = tx
+        .input
+        .iter()
+        .filter_map(|tx_in| {
+            let outpoint = OutPoint {
+                txid: tx_in.previous_output.txid.to_vec(),
+                vout: tx_in.previous_output.vout,
+            };
+            outpoint_to_index.get(&outpoint).cloned()
+        })
+        .collect();
+
+    Ok((tx.serialize(), used_utxo_indices))
+}
+
+#[query]
+#[candid_method(query)]
+fn sign_transaction(
+    private_key_wif: String,
+    serialized_transaction: Vec<u8>,
+    source_address: String,
+) -> Result<Vec<u8>, SignTransactionError> {
+    let private_key = PrivateKey::from_wif(&private_key_wif)
+        .map_err(|_| SignTransactionError::InvalidPrivateKeyWif)?;
+    let source_address = Address::from_str(&source_address)
+        .map_err(|_| SignTransactionError::MalformedSourceAddress)?;
+    let tx: Transaction = deserialize(serialized_transaction.as_slice())
+        .map_err(|_| SignTransactionError::MalformedTransaction)?;
+    Ok(common::sign_transaction(tx, private_key, source_address).serialize())
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn check_candid_interface_compatibility() {
+        use candid::types::subtype::{subtype, Gamma};
+        use candid::types::Type;
+        use ic_cdk::export::candid::{self};
+        use std::io::Write;
+        use std::path::PathBuf;
+
+        candid::export_service!();
+
+        let actual_interface = __export_service();
+        println!("Generated DID:\n {}", actual_interface);
+        let mut tmp = tempfile::NamedTempFile::new().expect("failed to create a temporary file");
+        write!(tmp, "{}", actual_interface).expect("failed to write interface to a temporary file");
+        let (mut env1, t1) =
+            candid::pretty_check_file(tmp.path()).expect("failed to check generated candid file");
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("candid.did");
+        let (env2, t2) =
+            candid::pretty_check_file(path.as_path()).expect("failed to open candid.did file");
+
+        let (t1_ref, t2) = match (t1.as_ref().unwrap(), t2.unwrap()) {
+            (Type::Class(_, s1), Type::Class(_, s2)) => (s1.as_ref(), *s2),
+            (Type::Class(_, s1), s2 @ Type::Service(_)) => (s1.as_ref(), s2),
+            (s1 @ Type::Service(_), Type::Class(_, s2)) => (s1, *s2),
+            (t1, t2) => (t1, t2),
+        };
+
+        let mut gamma = Gamma::new();
+        let t2 = env1.merge_type(env2, t2);
+        subtype(&mut gamma, &env1, t1_ref, &t2)
+            .expect("canister interface is not compatible with the candid.did file");
+    }
+}
